45c754af75ad772d23b3032ea0fe0d12ec255aea
callarfaniv <79884084+callarfaniv@users.noreply.github.com>
2021-03-01 20:36:20 -0600

    Add files via upload

--- App.js
+++ App.js
@@ -1,21 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Inicio from './src/screens/Inicio';
+import Login from './src/screens/Login';
+import Registro from './src/screens/Registro';
+
+const Stack = createStackNavigator();
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <NavigationContainer>
+            <Stack.Navigator initialRouteName='Inicio'>
+                <Stack.Screen name='Inicio' component={Inicio} />
+                <Stack.Screen name='Login' component={Login} />
+                <Stack.Screen name='Registro' component={Registro} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
---
+++ Registro.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const Registro = (props) => {
+    return (
+        <View>
+            <Text>Registro.js</Text>
+        </View>
+    );
+};
+
+export default Registro;
